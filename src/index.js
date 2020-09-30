fca029ce0f64d35fb1c43b1dca046b32c67feb37
wascoyur <mytem20007@gmail.com>
2020-09-30 10:57:47 +0500

    finish

--- src/index.js
+++ src/index.js
@@ -22,5 +22,14 @@ exports.max = function max (array) {
 
 exports.avg = function avg (array) {
   let out  = 0;
-  return 0;
+  if (typeof (array) === 'undefined' || array.length === 0) {
+      return out;
+  }
+  out = array.reduce((acc, el ) => {
+    return acc + el;
+  },0);
+  out = out/array.length;
+  // console.log('out:', out);
+
+  return out;
 }
