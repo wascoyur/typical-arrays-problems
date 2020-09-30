01ed545e429939bd82cbd7acf9da8aebca703cd4
wascoyur <mytem20007@gmail.com>
2020-09-30 10:39:05 +0500

    feat: min max is ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  // console.log('input:', array);
+  let out = 0;
+  if (typeof (array) === 'undefined' || array.length === 0) {
+    return out;
+  }
+  out = array.sort((a, b) => a - b)[0];
+  // console.log('out:', out);
+  return out;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let out = 0;
+  if (typeof (array) === 'undefined' || array.length === 0) {
+      return out;
+  }
+  out = array.sort((a, b) => b - a)[0];
+  // console.log('out:', out);
+  return out;
 }
 
 exports.avg = function avg (array) {
+  let out  = 
   return 0;
 }
